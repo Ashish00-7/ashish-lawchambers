8caceac7fbcdd524f57e707426e56b7a88fedc95
Ashish00-7 <ashishj639@gmail.com>
2025-06-11 19:58:02 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,8 @@
+
+function acceptDisclaimer() {
+    document.getElementById('disclaimer-popup').style.display = 'none';
+}
+
+function declineDisclaimer() {
+    window.location.href = "denied.html";
+}
